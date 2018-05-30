c41186c2f6c873f9a482beaf271b55f1defb5595
Thomas Gardiner <thomgardiner@gmail.com>
2018-05-30 12:03:10 -0600

    user end functionality, database update

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,9 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+let id = null;
+
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -11,12 +14,79 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  end();
 });
 
 const getTable = function() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
-      console.log(res);
+      for(i=0; i < res.length; i++){
+        console.log("ID: " + res[i].id + " | " + "Product Name: " + res[i].product_name + " | " + "Current Price: " + res[i].price);
+    }
+      promptID();
     });
 };
+
+const checkQuanity = function(id, quanity){
+    let query = connection.query(
+        "SELECT * FROM products WHERE ?",
+        [
+            {
+              id: id
+            }
+          ],
+          function(err, res) {
+            if(quanity < res[0].stock){
+                let total = res[0].price * quanity;
+                console.log("Your purchase comes to $" + total);
+                updateQuanity(id, quanity);
+            }
+            else{
+                console.log("There's not enough!");
+                connection.end();
+            }
+          }
+        );
+}
+
+const updateQuanity = function(id, amount) {
+    let query = connection.query(
+      "UPDATE products SET stock = stock - " + amount + " WHERE ?",
+      [
+        {
+          id: id
+        }
+      ],
+      function(err, res) {
+        // console.log(res.affectedRows + " stock updated!");
+        connection.end();
+      }
+    );
+}
+
+let promptID = function(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "userInput",
+          message: "What is the ID of the item you want to buy?"
+        }]).then(function(response){
+            id = response.userInput;
+            console.log("You said " + id);
+            promptQuanity();
+      })
+}
+
+let promptQuanity = function(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "userInput",
+          message: "How many?"
+        }]).then(function(response){
+            let quanity = response.userInput;
+            console.log("You said " + quanity);
+            checkQuanity(id, quanity);
+      })
+}
+
+getTable();
--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ USE bamazon;
 CREATE TABLE products (
 	id INT(11) NOT NULL,
 	product_name VARCHAR(100),
-	deparment_name VARCHAR(100),
-	price INT(11),
+	department_name VARCHAR(100),
+	price DECIMAL(11, 2),
 	stock INT(11),
 	PRIMARY KEY (id)
 );
